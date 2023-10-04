da45c1a9a6fab3f4f30992b195a1f64d1f927864
ivandmn <ivandmn1@gmail.com>
2023-10-04 19:07:36 +0200

    fix

--- AdminUsers.php
+++ AdminUsers.php
@@ -9,6 +9,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
+
     <body style="background-color:#f5f5f5;">
     <div class="container-fluid">
     <?php 
